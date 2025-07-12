37b518ac454166c85e3f0f0fb07b0e60db6003ac
Alifriduwan <6510110565@email.psu.ac.th>
2025-07-12 16:24:37 +0700

    user router login

--- app/routers/user_router.py
+++ app/routers/user_router.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException
+from fastapi import APIRouter, Depends, HTTPException , status
 from sqlmodel.ext.asyncio.session import AsyncSession
 from sqlmodel import select
 import datetime
 
 from app.models.user_model import (
-    DBUser, RegisteredUser, User, 
+    DBUser, RegisteredUser, User, Login
 )
 
 from ..models import get_session
@@ -39,3 +39,14 @@ async def register(user_in: RegisteredUser, session: AsyncSession = Depends(get_
     await session.refresh(user)
     return user
 
+
+@router.post("/login")
+async def login(login_in: Login, session: AsyncSession = Depends(get_session)):
+    q = select(DBUser).where(
+        (DBUser.email == login_in.identifier) | (DBUser.phone_number == login_in.identifier)
+    )
+    result = await session.exec(q)
+    user = result.first()
+    if not user or not user.verify_password(login_in.password):
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Invalid credentials")
+    return {"message": "Login success", "user_id": user.id}
